54a819e13be81eb14577cd4aa97a0c0528dbb8a9
Mainul-git <u1902141@student.cuet.ac.bd>
2020-11-15 20:07:50 +0600

    Home-page-apartment

--- src/components/Home/ApartmentCard/ApartmentCard.js
+++ src/components/Home/ApartmentCard/ApartmentCard.js
@@ -4,13 +4,13 @@ import bathroom from'../../../logos/bath 1.png'
 import bedroom from'../../../logos/bed 1.png'
 import ApartmentCardChild from '../ApartmentCardChild/ApartmentCardChild';
 
-const Apartment=[{img:"https://i.ibb.co/0rFr4pd/Rectangle-394.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}` ,price:194},
-{img:"https://i.ibb.co/WgXC5cP/Rectangle-396.png",title:"Family Apartment Three",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:356 },
+const Apartment=[{id:1,img:"https://i.ibb.co/0rFr4pd/Rectangle-394.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}` ,price:194},
+{id:2,img:"https://i.ibb.co/WgXC5cP/Rectangle-396.png",title:"Family Apartment Three",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:356 },
 
-{img:"https://i.ibb.co/P1tTtLj/Rectangle-398.png",title:"Gorgeous House",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:256  },
-{img:"https://i.ibb.co/8rtyhF3/Rectangle-405.png" ,title:"Luxury Villa",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:345},
-{img:"https://i.ibb.co/17MqxBf/Rectangle-403.png",title: "Epic Huda Palacio",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:536},
-{img:"https://i.ibb.co/7V4CrwM/Rectangle-404.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:283}]
+{ id:3,img:"https://i.ibb.co/P1tTtLj/Rectangle-398.png",title:"Gorgeous House",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:256  },
+{ id:4,img:"https://i.ibb.co/8rtyhF3/Rectangle-405.png" ,title:"Luxury Villa",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:345},
+{id:5,img:"https://i.ibb.co/17MqxBf/Rectangle-403.png",title: "Epic Huda Palacio",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:536},
+{id:6,img:"https://i.ibb.co/7V4CrwM/Rectangle-404.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:283}]
 const ApartmentCard = () => {
     return (
         <div>
