78f8eb34e1ba42e184f195f2a25f90211af86410
FAHIM AHMED <fahimfahad35@gmail.com>
2020-11-17 13:17:01 +0600

    home page dynamic

--- src/components/HomePage/ApartmentCard/ApartmentCard.js
+++ src/components/HomePage/ApartmentCard/ApartmentCard.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import location from"../../../logos/map-marker-alt-solid 1.png"
-import bathroom from'../../../logos/bath 1.png'
-import bedroom from'../../../logos/bed 1.png'
+import React, { useEffect, useState } from 'react';
+import loading from'../../../logos/loading.gif'
 import ApartmentCardChild from '../ApartmentCardChild/ApartmentCardChild';
-
-const Apartment=[{id:1,img:"https://i.ibb.co/0rFr4pd/Rectangle-394.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}` ,price:194},
-{id:2,img:"https://i.ibb.co/WgXC5cP/Rectangle-396.png",title:"Family Apartment Three",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:356 },
-
-{ id:3,img:"https://i.ibb.co/P1tTtLj/Rectangle-398.png",title:"Gorgeous House",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:256  },
-{ id:4,img:"https://i.ibb.co/8rtyhF3/Rectangle-405.png" ,title:"Luxury Villa",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:345},
-{id:5,img:"https://i.ibb.co/17MqxBf/Rectangle-403.png",title: "Epic Huda Palacio",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:536},
-{id:6,img:"https://i.ibb.co/7V4CrwM/Rectangle-404.png",title:"Washington Avenue",location:`${location}`,bathroom:`${bathroom}`,bedroom:`${bedroom}`,price:283}]
 const ApartmentCard = () => {
+    const[apartments,setApartments]=useState([])
+    useEffect(() =>{
+        fetch('http://localhost:5000/allapartment')
+        .then(response =>response.json())
+        .then(data =>{
+            if(data){
+                setApartments(data)
+                console.log(data)
+            }
+        })
+    },[])
     return (
         <div style={{backgroundColor:' #E5E5E5'}} className="pt-3">
             <div className="flex-column mb-3 text-center">
@@ -20,8 +21,8 @@ const ApartmentCard = () => {
                 <h2>available today</h2>
             </div>
             <section className="row w-75 m-auto ">
-            {
-                    Apartment.map(apartment => <ApartmentCardChild apartment={apartment} key={apartment.title}></ApartmentCardChild >)
+                {
+                    apartments.length===0?<img src={loading} alt=""/>:apartments.map(apartment=><ApartmentCardChild apartment={apartment} key={apartment._id}></ApartmentCardChild>)
                 }
             </section>
         </div>
--- src/components/HomePage/ApartmentCardChild/ApartmentCardChild.js
+++ src/components/HomePage/ApartmentCardChild/ApartmentCardChild.js
@@ -1,24 +1,26 @@
 import React from 'react';
-
-const ApartmentCardChild = ({apartment}) => {
-    return (<div className="col-md-4 mb-3 container ">
-        <div className="card  " >
-        <img src={apartment.img} className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h4 className="card-title">{apartment.title}</h4>
-          <div className="d-flex"><img className="mr-1" style={{height:"30px"}} src={apartment.location}/>
-          <p>Nasirabad H/S,Chattogram</p></div>
-          <div className="row">
-<div className="d-flex col-md-6"><img className="mr-1" style={{height:'20px'}} src={apartment.bedroom}/><p>3 bedrooms</p></div>
-<div className="d-flex col-md-6"><img  className="mr-1"style={{height:'20px'}}src={apartment.bathroom}/><p>2 bathroom</p></div>
-          </div><div className="d-flex ">
-  <h4 style={{fontWeight:'bolder',fontSize:"30px",color:"#275A53"}}  className="mr-5">${apartment.price}</h4>
-          <button className="btn text-white ml-4  " type="button" id="button-addon2" style={{backgroundColor:"#275A53"}}>View Details</button></div>
-        </div>
+import locationLogo from"../../../logos/map-marker-alt-solid 1.png"
+import bathroomLogo from'../../../logos/bath 1.png'
+import bedroomLogo from'../../../logos/bed 1.png'
+const ApartmentCardChild = ({ apartment }) => {
+  return (<div className="col-md-4 mb-3 container ">
+    <div className="card  " >
+      <img src={`data:image/png;base64,${apartment.Primaryimage.img}`} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h4 className="card-title">{apartment.title}</h4>
+        <div className="d-flex"><img className="mr-1" style={{ height: "30px" }} src={locationLogo} />
+          <p>{apartment.location}</p></div>
+        <div className="row">
+          <div className="d-flex col-md-6"><img className="mr-1" style={{ height: '20px' }} src={bedroomLogo} /><p>{apartment.bedroom} bedrooms</p></div>
+          <div className="d-flex col-md-6"><img className="mr-1" style={{ height: '20px' }} src={bathroomLogo} /><p>{apartment.bathroom} bathrooms</p></div>
+        </div><div className="d-flex ">
+          <h4 style={{ fontWeight: 'bolder', fontSize: "30px", color: "#275A53" }} className="mr-5">${apartment.price}</h4>
+          <button className="btn text-white ml-4  " type="button" id="button-addon2" style={{ backgroundColor: "#275A53" }}>View Details</button></div>
       </div>
-      </div>
+    </div>
+  </div>
 
-    );
+  );
 };
 
 export default ApartmentCardChild;
